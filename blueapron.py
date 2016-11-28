c28723306ba64159e767e1c629a5b1e423e9bc19
Chuck Smith <smith.chuck@gmail.com>
2016-11-28 13:26:37 -0800

    Wrote a first pass of delivery status change detection.

--- blueapron.py
+++ blueapron.py
@@ -1,7 +1,24 @@
+import urllib
+from bs4 import BeautifulSoup
 
+URL = "http://www.ontrac.com/trackingres.asp?tracking_number=D10011045155761&x=12&y=16"
 
 def main():
-  pass
+  page = urllib.urlopen(URL)
+  soup = BeautifulSoup(page, "html.parser")
+  trs = soup.find_all("tr")
+  for tr in trs:
+    #   print tr
+      try:
+          if tr.td.b.text == "Delivery Status:":
+              status = tr.find_all("td")[1].text.strip()
+              if status != "IN TRANSIT DETAILS":
+                  print "Status has changed."
+              else:
+                  print "No status change.."
+
+      except Exception as e:
+          pass
 
 if __name__ == "__main__":
   main()
