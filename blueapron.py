5b4c809283364ab935330e816d6ae2b7fd4d8b4b
Chuck Smith <smith.chuck@gmail.com>
2016-11-28 16:12:13 -0800

    Added some comments. Increased specificity of the Exception being handled while the parser is iterating.

--- blueapron.py
+++ blueapron.py
@@ -4,8 +4,16 @@ import os
 
 URL = "http://www.ontrac.com/trackingres.asp?tracking_number=D10011045155761&x=12&y=16"
 PB_API = "POST https://api.pushbullet.com/v2/pushes"
+
+####
+# This token is not available in GitHub because it's uniquely tied to my own
+# Pushbullet account. Limited API access is freely avaialble by finding
+# your account's token over here: https://www.pushbullet.com/#settings/account.
+####
 PB_TOKEN = open("pushbullet_access_token.txt").read().strip() # no EOL characters
 
+### This POST pushes notifications to all devices registered to the account.
+##  That may or may not be the desired effect, so proceed with caution.
 COMMAND = ("""curl --header 'Access-Token:%s' \
      --header 'Content-Type: application/json' \
      --data-binary '{"body":"Blue Apron has been marked as delivered", \
@@ -20,7 +28,7 @@ def get_status(page):
             if tr.td.b.text == "Delivery Status:":
               status = tr.find_all("td")[1].text.strip()
               return status
-        except Exception as e:
+        except AttributeError as e:
             pass
 
 def main():
